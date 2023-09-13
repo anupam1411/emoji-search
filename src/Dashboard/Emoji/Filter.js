bff49be76f2f132735b338550e77aa48613744c6
Anupam Sagar <sagaranupam2@gmal.com>
2023-09-13 22:25:01 +0530

    rename

--- src/Dashboard/Emoji/Filter.js
+++ src/Dashboard/Emoji/Filter.js
@@ -1,19 +1,17 @@
-import emojiList from "./emojiList.json";
+import EmojiList from "./EmojiList.json";
 
 function Filter(searchText, maxResults) {
-  return emojiList
-    .filter((emoji) => {
-      if (
-        emoji.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
-      ) {
-        return true;
-      }
-      if (emoji.keywords.includes(searchText)) {
-        return true;
-      }
-      return false;
-    })
-    .slice(0, maxResults);
+  return EmojiList.filter((emoji) => {
+    if (
+      emoji.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
+    ) {
+      return true;
+    }
+    if (emoji.keywords.includes(searchText)) {
+      return true;
+    }
+    return false;
+  }).slice(0, maxResults);
 }
 
 export default Filter;
